60692fab329f1f73c67d04ad973fd3b279697286
joelnyongesa <joelnyongesa148@gmail.com>
2023-12-27 23:30:19 +0300

    data: added data about my projects

---
+++ src/data/projects.js
@@ -0,0 +1,37 @@
+import GameHaven from '../images/game-haven.avif';
+
+export const projectsData = [
+    {
+        id: 1,
+        title: 'Game Haven',
+        category: 'Web Application',
+        img: GameHaven,
+        deployedlink: 'game-haven.netlify.app',
+        githubLink: ''
+    },
+    // Add more projects here in future
+    {
+        id: 2,
+        title: 'Game Haven',
+        category: 'Web Application',
+        img: GameHaven,
+        deployedlink: 'game-haven.netlify.app',
+        githubLink: ''
+    },
+    {
+        id: 3,
+        title: 'Game Haven',
+        category: 'Web Application',
+        img: GameHaven,
+        deployedlink: 'game-haven.netlify.app',
+        githubLink: ''
+    },
+    {
+        id: 4,
+        title: 'Game Haven',
+        category: 'Web Application',
+        img: GameHaven,
+        deployedlink: 'game-haven.netlify.app',
+        githubLink: ''
+    },
+]
