6cbc038f8144ec37f9ff8b19fbf8cce0c7a63466
joelnyongesa <joelnyongesa148@gmail.com>
2023-12-27 23:30:01 +0300

    data: added data about me

---
+++ src/data/aboutMeData.js
@@ -0,0 +1,10 @@
+export const aboutMeData = [
+    {
+        id: 1,
+        bio: "I am a full-stack software developer with proficiency in JavaScript and Python. I have worked with frameworks including React, Express and Flask."
+    },
+    {
+        id: 2,
+        bio: "I can help you create a product, feature, or website. Browse my portfolio and experience, and don't hesitate to get in touch with me if you like what you see and need a project completed!"
+    }
+]
